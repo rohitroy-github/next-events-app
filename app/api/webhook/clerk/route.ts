5767abcd81ce648db42aceee20a3a957f90f61dd
rohit <rhtry.tech@gmail.com>
2024-01-19 15:17:24 +0530

    initial commit

--- app/api/webhook/clerk/route.ts
+++ app/api/webhook/clerk/route.ts
@@ -62,6 +62,8 @@ export async function POST(req: Request) {
       clerkId: id,
       email: email_addresses[0].email_address,
       username: username!,
+      // firstName: first_name,
+      // lastName: last_name,
       photo: image_url,
     };
 
@@ -75,7 +77,7 @@ export async function POST(req: Request) {
     //   });
     // }
 
-    return NextResponse.json({message: "OK", user: user});
+    // return NextResponse.json({message: "OK", user: newUser});
   }
 
   return new Response("", {status: 200});
