ebefecc47fb3193084bd5f6c0c0385e7bd12ca6c
rohit <rhtry.tech@gmail.com>
2024-01-19 14:44:24 +0530

    initial commit

--- app/api/webhook/clerk/route.ts
+++ app/api/webhook/clerk/route.ts
@@ -55,8 +55,6 @@ export async function POST(req: Request) {
   const {id} = evt.data;
   const eventType = evt.type;
 
-  // console.log(evt.type);
-
   if (eventType === "user.created") {
     const {id, email_addresses, image_url, first_name, last_name, username} =
       evt.data;
