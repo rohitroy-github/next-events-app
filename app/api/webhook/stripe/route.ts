f780c6054bb5b9af51fa9f8bff5d91a02c4614cd
rohit <rhtry.tech@gmail.com>
2024-01-25 12:34:44 +0530

    add : stripe functions

--- app/api/webhook/stripe/route.ts
+++ app/api/webhook/stripe/route.ts
@@ -26,10 +26,12 @@ export async function POST(request: Request) {
     const order = {
       stripeId: id,
       eventId: metadata?.eventId || "",
-      buyerId: metadata?.buyerId || "",
+      buyerId: metadata?.buyerIid || "",
       totalAmount: amount_total ? (amount_total / 100).toString() : "0",
       createdAt: new Date(),
     };
+
+    console.log(order);
 
     const newOrder = await createOrder(order);
     return NextResponse.json({message: "OK", order: newOrder});
